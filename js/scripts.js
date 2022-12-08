7b8fa4035eccf913b264b26437ee48921b298eb0
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-08 13:44:30 -0800

    Added pizzaPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,32 @@ Pizza.prototype.removeTopping = function (topping) {
 Pizza.prototype.updateSize = function (pizzaSize) {
   this.size = pizzaSize;
 }
+
+Pizza.prototype.pizzaPrice = function () {
+  let basePrice = 5;
+  let sizeMultiplier = 0;
+  let toppingsPrice = 0;
+  let pizzaPrice = 0;
+
+  if (this.size === "small") {
+    sizeMultiplier = 1;
+  } else if (this.size === "medium") {
+    sizeMultiplier = 2;
+  } else if (this.size === "large") {
+    sizeMultiplier = 3;
+  } else if (this.size === "extra-large") {
+    sizeMultiplier = 4;
+    console.log("extra large")
+  }
+
+  toppingsPrice = this.toppings.length * 0.75;
+  pizzaPrice = (basePrice + toppingsPrice) * sizeMultiplier;
+  console.log(pizzaPrice);
+}
+
 //test cases
 let myPizza = new Pizza();
 myPizza.addTopping("anchovies");
 myPizza.addTopping("anchovies");
 myPizza.addTopping("Peppers");
-myPizza.updateSize("small");
+myPizza.updateSize("extra-large");
