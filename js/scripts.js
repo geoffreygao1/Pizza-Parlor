0e2585744bd45514abcf6ab3884e6600bfe48aae
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-08 13:35:04 -0800

    Added updateSize function

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,13 @@ Pizza.prototype.addTopping = function (topping) {
 Pizza.prototype.removeTopping = function (topping) {
   this.toppings.splice(this.toppings.indexOf(topping), 1);
 }
+
+Pizza.prototype.updateSize = function (pizzaSize) {
+  this.size = pizzaSize;
+}
 //test cases
 let myPizza = new Pizza();
 myPizza.addTopping("anchovies");
 myPizza.addTopping("anchovies");
 myPizza.addTopping("Peppers");
+myPizza.updateSize("small");
