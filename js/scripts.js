38e67689b5a4a946294b7c347af8321dcc15bf7a
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-08 13:33:58 -0800

    Added addTopping and removeTopping functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function Pizza() {
   this.size = "";
 };
 
+Pizza.prototype.addTopping = function (topping) {
+  this.toppings.push(topping);
+};
+
+Pizza.prototype.removeTopping = function (topping) {
+  this.toppings.splice(this.toppings.indexOf(topping), 1);
+}
+//test cases
+let myPizza = new Pizza();
+myPizza.addTopping("anchovies");
+myPizza.addTopping("anchovies");
+myPizza.addTopping("Peppers");
